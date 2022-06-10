5e968248a4f23e54ffaaabd455f14aa6478a3ac4
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2022-06-10 07:33:57 -0600

    feat: configure routing with react-router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,31 @@
-import React from "react";
+import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route, Switch } from 'react-router-dom';
 
-const App = () => {
-  const hello = "yessir";
-  return <div>{hello}</div>;
-};
+import { Actors, MovieInformation, Movies, NavBar, Profile } from '.';
+
+const App = () => (
+  <div>
+    <CssBaseline />
+    <h1>Filmpire Webapp</h1>
+    <NavBar />
+    <main>
+      <Switch>
+        <Route exact path="/">
+          <Movies />
+        </Route>
+        <Route path="/movie/:id">
+          <MovieInformation />
+        </Route>
+        <Route path="/actors/:id">
+          <Actors />
+        </Route>
+        <Route path="/profile/:id">
+          <Profile />
+        </Route>
+      </Switch>
+    </main>
+  </div>
+);
 
 export default App;
